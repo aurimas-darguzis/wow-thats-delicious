97c12ed0f6602bde92bab5f831fcefa5c596cd88
aurimas-darguzis <aurimas@darguzis.com>
2017-05-16 15:13:31 +0100

    added post method to /add route

--- routes/index.js
+++ routes/index.js
@@ -4,15 +4,6 @@ const storeController = require('../controllers/storeController')
 
 router.get('/', storeController.homePage)
 router.get('/add', storeController.addStore)
+router.post('/add', storeController.createStore)
 
 module.exports = router
-
-/* factorial in recursive manner */
-
-// it's not the part of this course, but
-// hey, it is still good to know, right?
-
-function factorial (n) {
-  if (n < 2) { return 1 }
-  return n * factorial(n - 1)
-}
