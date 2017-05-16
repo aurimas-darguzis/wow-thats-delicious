a833c1e4f0d7ebdc39e55456b4f70e3ff480eeac
aurimas-darguzis <aurimas@darguzis.com>
2017-05-16 20:48:15 +0100

    storing to db

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const storeController = require('../controllers/storeController')
-const { catchError } = require('../handlers/errorHandlers')
+const { catchErrors } = require('../handlers/errorHandlers')
 
 router.get('/', storeController.homePage)
 router.get('/add', storeController.addStore)
-router.post('/add', catchError(storeController.createStore))
+router.post('/add', catchErrors(storeController.createStore))
 
 module.exports = router
