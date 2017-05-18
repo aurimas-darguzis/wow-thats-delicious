78627e42e06a2bf6f7938572db0082daceed9e09
aurimas-darguzis <aurimas@darguzis.com>
2017-05-18 21:17:22 +0100

    added properties to Schema for location, coordinates and address

--- models/Store.js
+++ models/Store.js
@@ -18,7 +18,23 @@ const storeSchema = new mongoose.Schema({
     type: String,
     trim: true
   },
-  tags: [String]
+  tags: [String],
+  created: {
+    type: Date,
+    default: Date.now
+  },
+  location: {
+    type: String,
+    default: 'Point'
+  },
+  coordinates: [{
+    type: Number,
+    required: 'You must supply coordinates!'
+  }],
+  address: {
+    type: String,
+    required: 'You must suplly an address!'
+  }
 })
 
 // before we save schema, we want to prepopulate slug.
