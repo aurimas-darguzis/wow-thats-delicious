02816d6676da8d7cb9aa406634c02fb7075c5135
aurimas-darguzis <aurimas@darguzis.com>
2017-05-19 14:40:31 +0100

    getStoreBySlug fn in storeController. Also route is set up

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -83,6 +83,14 @@ exports.updateStore = async (req, res) => {
   res.redirect(`/stores/${store._id}/edit`)
 }
 
+exports.getStoreBySlug = async (req, res, next) => {
+  const store = await Store.findOne({ slug: req.params.slug })
+  if (!store) {
+    return next()
+  }
+  res.render('store', { store, title: store.name })
+}
+
 exports.deleteStore = async (req, res) => {
   const store = await Store.findOneAndRemove({ _id: req.params.id })
 }
