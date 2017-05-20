8a5be54c9f029f903678b574c1d141fc509cfa42
aurimas-darguzis <aurimas@darguzis.com>
2017-05-20 20:32:40 +0100

    added validateRegister middleware

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,3 +7,25 @@ exports.loginForm = (req, res) => {
 exports.registerForm = (req, res) => {
   res.render('register', { title: 'Register' })
 }
+
+exports.validateRegister = (req, res, next) => {
+  req.sanitizeBody('name')
+  req.checkBody('name', 'You must suplly a name!').notEmpty()
+  req.checkBody('email', 'That Email is not valid!').isEail()
+  // lookup into express validator for properties to validate
+  req.sanitizeBody('email').normalizeEmail({
+    remove_dots: false,
+    remove_extension: false,
+    gmail_remove_subaddress: false
+  })
+  req.checkBody('password', 'Password cannot be blank!').notEmpty()
+  req.checkBody('password-confirm', 'Confirmed password cannot be blank!').notEmpty()
+  req.checkBody('password-confirm', 'Oops! Your passwords do not math').equals(req.body.password)
+
+  const errors = req.validationErrors()
+  if (errors) {
+    req.flash('error', errors.map(err => err.msg))
+    res.render('register', { title: 'Register', body: req.body,
+        flashes: req.flash()})
+  }
+}
