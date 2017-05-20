da3eba28f22c551f37fa8d7f3e9bc5b27f9a2d55
aurimas-darguzis <aurimas@darguzis.com>
2017-05-20 19:03:37 +0100

    append -[number] to the slug, if the same name already exist

--- models/Store.js
+++ models/Store.js
@@ -43,13 +43,19 @@ const storeSchema = new mongoose.Schema({
 // before we save schema, we want to prepopulate slug.
 // so we take the value from name, pass it slug function that
 // we imported and assign to slug property
-storeSchema.pre('save', function (next) {
+storeSchema.pre('save', async function (next) {
   // `this` - is the store that we are trying to save
   if (!this.isModified('name')) {
     next() // skip it
     return // stop this function from running
   }
   this.slug = slug(this.name)
+  // find other stores that have a slug of name, name-1, name-2
+  const slugRegEx = new RegExp(`^(${this.slug})((-[0-9]*$)?)$`, 'i')
+  const storesWithSlug = await this.constructor.find({ slug: slugRegEx })
+  if (storesWithSlug.length) {
+    this.slug = `${this.slug}-${storesWithSlug.length + 1}`
+  }
   next()
   // TODO make more resiliant so slugs are unique
 })
