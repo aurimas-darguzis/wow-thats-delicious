f2298356d43c5b9adbb8eb60c0e7a4fda0466999
aurimas-darguzis <aurimas@darguzis.com>
2017-05-25 22:17:42 +0100

    sunday jazz

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const storeController = require('../controllers/storeController')
-const userController = require('../controllers/userController')
+const storeController = require('../controllers/storeController')   
+const userController = require('../controllers/userController')   
 const authController = require('../controllers/userController')
-const { catchErrors } = require('../handlers/errorHandlers')
+const { catchErrors } = require('../handlers/errorHandlers')   
 
 router.get('/', catchErrors(storeController.getStores))
 router.get('/stores', catchErrors(storeController.getStores))
