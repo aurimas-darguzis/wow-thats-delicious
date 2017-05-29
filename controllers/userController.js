951f520eee908e8315949dc269f63ef5c7a36f4a
aurimas-darguzis <aurimas@darguzis.com>
2017-05-29 13:53:58 +0100

    added updateAccount method into userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -45,3 +45,10 @@ exports.register = async (req, res, next) => {
 exports.account = (req, res) => {
   res.render('account', { title: 'Edit your account' })
 }
+
+exports.updateAccount = async (req, res) => {
+  const updates = {
+    name: req.body.name,
+    email:req.body.email
+  }
+}
