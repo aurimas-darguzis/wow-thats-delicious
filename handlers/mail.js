100eb96414e2087c8f4f32c6236235bf1ff0559e
aurimas-darguzis <aurimas@darguzis.com>
2017-06-10 14:39:57 +0100

    generate pug into html and pass along to mail options

--- handlers/mail.js
+++ handlers/mail.js
@@ -13,12 +13,18 @@ const transport = nodemailer.createTransport({
   }
 });
 
+const generateHTML = (filename, options = {}) => {
+  const html = pug.renderFile(`${__dirname}/../views/email/${filename}.pug`, options);
+  return html;
+}
+
 exports.send = async (options) => {
+  const html = generateHTML(options.filename, options);
   const mailOptions = {
     from: `Me name <noreply@name.com>`,
     to: options.user.email,
     subject: options.subject,
-    html: 'This will be filled in later',
+    html,
     text: 'This will also be filled in later'
   }
 
