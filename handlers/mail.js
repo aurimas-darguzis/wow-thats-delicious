d7e0325a1eb7269b8835a4884c0ac2266cc64e8d
aurimas-darguzis <aurimas@darguzis.com>
2017-06-11 15:07:22 +0100

    linted code

--- handlers/mail.js
+++ handlers/mail.js
@@ -1,8 +1,8 @@
-const nodemailer = require('nodemailer');
-const pug = require('pug');
-const juice = require('juice');
-const htmlToText = require('html-to-text');
-const promisify = require('es6-promisify');
+const nodemailer = require('nodemailer')
+const pug = require('pug')
+const juice = require('juice')
+const htmlToText = require('html-to-text')
+const promisify = require('es6-promisify')
 
 const transport = nodemailer.createTransport({
   host: process.env.MAIL_HOST,
@@ -11,17 +11,17 @@ const transport = nodemailer.createTransport({
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS
   }
-});
+})
 
 const generateHTML = (filename, options = {}) => {
-  const html = pug.renderFile(`${__dirname}/../views/email/${filename}.pug`, options);
+  const html = pug.renderFile(`${__dirname}/../views/email/${filename}.pug`, options)
   const inlined = juice(html)
-  return inlined;
+  return inlined
 }
 
 exports.send = async (options) => {
-  const html = generateHTML(options.filename, options);
-  const text = htmlToText.fromString(html);
+  const html = generateHTML(options.filename, options)
+  const text = htmlToText.fromString(html)
 
   const mailOptions = {
     from: `Me name <noreply@name.com>`,
