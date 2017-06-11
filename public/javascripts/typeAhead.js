56f3f13858099cfd0dbb4ec2fc366ffeafa0445e
aurimas-darguzis <aurimas@darguzis.com>
2017-06-11 22:06:22 +0100

    search results giving html. working on up down and enter

--- public/javascripts/typeAhead.js
+++ public/javascripts/typeAhead.js
@@ -1,14 +1,54 @@
 const axios = require('axios');
 
+function searchResultsHTML(stores) {
+  return stores.map(store => {
+    return `
+      <a href="/store/${store.slug}" className="search__result">
+        <strong>${store.name}</strong>
+      </a>
+    `
+  }).join('')
+}
+
 function typeAhead(search) {
   if (!search) {
     return;
   }
 
   const searchInput = search.querySelector('input[name="search"]')
-  const searchResult = search.querySelector('.search__result')  
-  
+  const searchResults = search.querySelector('.search__result')  
 
+  searchInput.on('input', function() {
+    // if there is no value, quit it!
+    if (!this.value) {
+      searchResults.style.display = 'none';
+      return;
+    }
+
+    // show the search results
+    searchResults.style.display = 'block';
+    searchResults.innerHTML = '';
+
+    axios
+      .get(`/api/search?q=${this.value}`)
+      .then(res => {
+        if (res.data.length) {
+          searchResults.innerHTML = searchResultsHTML(res.data)
+        }
+      })
+      .catch(err => {
+        console.error(err)
+      })
+  })
+
+  // handle keyboard inputs
+  searchInput.on('keyup', (e) => {
+    // if they aren't pressing up, down or enter, who cares!
+    if (![38, 40, 13].includes(e.keyCode)) {
+      return;
+    }
+    
+  })
 }
 
 export default typeAhead;
