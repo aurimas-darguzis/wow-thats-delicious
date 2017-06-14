b3d78ea89376042adfcbc3d760a06f77b8740a02
aurimas-darguzis <aurimas@darguzis.com>
2017-06-14 09:32:29 +0100

    pushing user data to API

--- public/javascripts/modules/heart.js
+++ public/javascripts/modules/heart.js
@@ -1,1 +1,20 @@
 import axios from 'axios'
+import { $ } from './bling'
+
+function ajaxHeart (e) {
+  e.preventDefault()
+  // this => form tag
+  axios.post(this.action)
+       .then(res => {
+          // this.heart => form has 'heart' attribute, in this case its a button
+          const isHearted = this.heart.classList.toggle('heart__button--heated')
+          $('.heart-count').textContent = res.data.hearts.length
+          if (isHearted) {
+            this.heart.classList.add('heart__button--float')
+            setTimeout(() => this.heart.classList.remove('heart__button--float'), 2500)
+          }
+       })
+       .catch(console.error)
+}
+
+export default ajaxHeart;
