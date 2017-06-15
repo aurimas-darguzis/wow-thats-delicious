3bd6536c28f00203d9901a6132080dd1a98da437
aurimas-darguzis <aurimas@darguzis.com>
2017-06-15 11:56:30 +0100

    check if user is Logged In before going to /hearts page

--- routes/index.js
+++ routes/index.js
@@ -51,7 +51,7 @@ router.post('/account/reset/:token',
 )
 
 router.get('/map', storeController.mapPage)
-router.get('/hearts', catchErrors(storeController.getHearts))
+router.get('/hearts', authController.isLoggedIn, catchErrors(storeController.getHearts))
 
 /* API */
 router.get('/api/search', catchErrors(storeController.searchStores))
